ca6420834d35cefeedacffb4cfa4a841e6c36168
Mike Delago <michaeldelago98@gmail.com>
2023-07-04 15:16:58 -0400

    extra tests

--- tests/main.lisp
+++ tests/main.lisp
@@ -4,12 +4,12 @@
         :rove))
 (in-package :cl-config/tests/main)
 
-
 (setup
   (defconfig nil
     (home :env "HOME")
     (unset-var :env "doesnt-exist" :default "default-value")
-    (kw-var :default "3" :key (lambda (x) (parse-integer x)))))
+    (int-var :default "3" :key (lambda (x) (parse-integer x)))
+    (kw-var :default "foo" :key (lambda (x) (intern (string-upcase x) :keyword)))))
 
 (deftest env-vars 
   (testing "Able to grab configuration from an environment variable"
@@ -17,4 +17,6 @@
   (testing "default values are resolved if env var is not found"
     (ok (equalp "default-value" (config-unset-var))))
   (testing "Able to perform type conversions on config values with the :key param"
-    (ok (eq 3 (config-kw-var)))))
+    (ok (eq 3 (config-int-var))))
+  (testing "Able to type convert keyword arguments"
+    (ok (eq :foo (config-kw-var)))))
